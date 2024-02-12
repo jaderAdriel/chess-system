f76ef2237a59d3aafe3e777e9ccef981e8f19bf5
jader <jaderadriel7@gmail.com>
2024-02-12 11:14:58 -0300

    fix: Board printing

--- src/application/UI.java
+++ src/application/UI.java
@@ -4,16 +4,16 @@ import chess.ChessPiece;
 
 public class UI {
     public static void printBoard(ChessPiece[][] pieces) {
-        int colIdentifier = 9;
-        for (int i = 0; i < pieces.length; i++) {
-            System.out.print(colIdentifier - (i + 1) + "  ");
+
+        for (int i = pieces.length - 1; i >= 0; i--) {
+            System.out.print(i +  1 + " ");
             for (int j = 0; j < pieces.length; j++) {
 
                 printPiece(pieces[i][j]);
             }
             System.out.println();
         }
-        System.out.print("   ");
+        System.out.print("  ");
         for (int i = 97; i <= 104; i++) {
             System.out.print((char) i + " ");
         }

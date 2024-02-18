50d85e8eefbd6b8003eec29275e2c58f014afb0e
jader <jaderadriel7@gmail.com>
2024-02-18 17:19:10 -0300

    Implementing possible moves of King

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -14,8 +15,34 @@ public class King extends ChessPiece {
         return "K";
     }
 
+    private void computeValidMoves(boolean[][] mat, int rowIncrement, int columnIncrement) {
+        Position p = new Position(position.getRow(), position.getColumn());
+
+        p.setValues(p.getRow() + rowIncrement, p.getColumn() + columnIncrement);
+
+        if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+    }
     @Override
     public boolean[][] possibleMoves() {
-        return new boolean[getBoard().getRows()][getBoard().getColumns()];
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(position.getRow() - 1, position.getColumn() - 1);
+
+        computeValidMoves(mat, 0, 1);
+        computeValidMoves(mat, 1, 1);
+        computeValidMoves(mat, -1, 1 );
+        computeValidMoves(mat, 0, -1);
+        computeValidMoves(mat, 1, -1);
+        computeValidMoves(mat, -1, -1);
+        computeValidMoves(mat, 1, 0);
+        computeValidMoves(mat, -1, 0);
+
+        return mat;
     }
 }

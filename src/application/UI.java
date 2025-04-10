91ab1502b31ba166c5f74e4f1cb2f95b5cc89186
jaderAdriel <jaderadriel7@gmail.com>
2025-04-10 01:54:43 -0300

    Refactor UI class for improved board rendering and piece display

--- src/application/UI.java
+++ src/application/UI.java
@@ -9,36 +9,52 @@ import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class UI {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
 
+    public static final String CHESS_COM_LIGHT_BG = "\033[48;5;151m"; // Verde claro (destaque)
+    public static final String CHESS_COM_DARK_BG  = "\033[48;5;71m";  // Verde escuro (destaque)
+
+    // Cores para ações
+    public static final String CHESS_COM_DARK_PLACE_TARGET_BG = "\033[48;5;191m"; // Destino selecionado (casa escura)
+    public static final String CHESS_COM_LIGHTER_PLACE_TARGET_BG = "\033[48;5;149m"; // Destino selecionado (casa clara)
+
+    public static final String LIGHTER_PLACE_TARGET_BG = CHESS_COM_LIGHTER_PLACE_TARGET_BG; // Destino selecionado (casa clara)
+    public static final String DARK_PLACE_TARGET_BG = CHESS_COM_DARK_PLACE_TARGET_BG; // Destino selecionado (casa escura)
+
+    // Peças pretas: cor preta forte
+    public static final String BLACK_PIECES_COLOR = "\033[1;30m";
+
+    // Peças brancas: cor branca forte
+    public static final String WHITE_PIECES_COLOR = "\033[1;97m";
+
+    // Casas do tabuleiro
+    public static final String DARKER_PLACE_COLOR = CHESS_COM_DARK_BG;
+    public static final String LIGHTER_PLACE_COLOR = CHESS_COM_LIGHT_BG;
+
+    // Outros
+    public static final String YELLOW_BOLD = "\033[1;93m";         // Amarelo vibrante
+    public static final String RED_BOLD = "\033[1;91m";            // Vermelho vibrante
+    public static final String RED_UNDERLINED = "\033[4;91m";      // Vermelho sublinhado
+    public static final String POSSIBLE_MOVE_COLOR = YELLOW_BOLD;
 
     // Bold High Intensity
     public static final String WHITE_BOLD_BRIGHT = "\033[1;97m";
 
-    public static final String RED_UNDERLINED = "\033[4;31m";    // RED
-
     public static final String YELLOW_BACKGROUND_BRIGHT = "\033[0;103m";// YELLOW
     public static final String BLACK_BOLD = "\033[1;30m";  // BLACK
-    public static final String RED_BOLD = "\033[1;31m";    // RED
 
     public static final String GRAY_BACKGROUND_BRIGHT = "\033[0;47m"; // Background cinza claro
     public static final String GRAY_BACKGROUND_NORMAL = "\033[0;100m"; // Background cinza escuro
 
-    public static final String BLACK_PIECES_COLOR = BLACK_BOLD;
-    public static final String WHITE_PIECES_COLOR = "\033[1;33m";
-    public static final String POSSIBLE_MOVE_COLOR = RED_BOLD;
-    public static final String WHITE_PLACE_BG = GRAY_BACKGROUND_BRIGHT;
-    public static final String BLACK_PLACE_BG = GRAY_BACKGROUND_NORMAL;
-    public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
-
     public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
+        System.out.print("\n\033[H\033[2J");
         System.out.flush();
     }
+
+
     public static ChessPosition readChessPosition(Scanner sc) {
         try {
             String position = sc.nextLine();
@@ -60,39 +76,27 @@ public class UI {
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
-        clearScreen();
-        for (int i = pieces.length - 1; i >= 0; i--) {
-            String nextBackgroundColor = (i % 2 != 0) ? WHITE_PLACE_BG : BLACK_PLACE_BG;
-
-            System.out.print(i +  1 + " ");
-            for (int j = 0; j < pieces.length; j++) {
-
-                String formarting = ANSI_RESET + nextBackgroundColor + " ";
-                printPiece(pieces[i][j], nextBackgroundColor,   false);
-
-                nextBackgroundColor = (nextBackgroundColor.equals(BLACK_PLACE_BG)) ? WHITE_PLACE_BG : BLACK_PLACE_BG;
-
-            }
-            System.out.println(ANSI_RESET);
-        }
-        System.out.print("   ");
-        for (int i = 97; i <= 104; i++) {
-            System.out.print((char) i + "  ");
-        }
+        boolean[][] arr = new boolean[pieces.length][pieces.length];
+        printBoard(pieces, arr );
     }
 
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
         clearScreen();
+
         for (int i = pieces.length - 1; i >= 0; i--) {
-            String nextBackgroundColor = (i % 2 != 0) ? WHITE_PLACE_BG : BLACK_PLACE_BG;
+            boolean isLighterPlace = (i % 2 != 0);
 
             System.out.print(i +  1 + " ");
 
             for (int j = 0; j < pieces.length; j++) {
-                printPiece(pieces[i][j], nextBackgroundColor, possibleMoves[i][j]);
+                boolean placeHasPossibleMove = false;
 
-                nextBackgroundColor = (nextBackgroundColor.equals(BLACK_PLACE_BG)) ? WHITE_PLACE_BG : BLACK_PLACE_BG;
+                if (possibleMoves[0].length == pieces[0].length && possibleMoves[1].length == pieces[1].length )
+                    placeHasPossibleMove = possibleMoves[i][j];
 
+                printPlace(isLighterPlace, pieces[i][j], placeHasPossibleMove);
+
+                isLighterPlace = !isLighterPlace;
             }
             System.out.println(ANSI_RESET);
         }
@@ -102,24 +106,64 @@ public class UI {
         }
     }
 
+    private static void printPlace(boolean isLighterPlace, ChessPiece piece, boolean hasPossibleMove) {
+        String placeholder = hasPossibleMove ? " •" : "  ";
 
-    private static void printPiece(ChessPiece piece, String backgroundColor, boolean isPossibleMove) {
-        String placeholder = isPossibleMove ? " •" : "  ";
-        String background = isPossibleMove ? YELLOW_BACKGROUND_BRIGHT :  backgroundColor;
+        String backgroundColor;
+
+        if (hasPossibleMove) {
+            backgroundColor = isLighterPlace ? LIGHTER_PLACE_TARGET_BG : DARK_PLACE_TARGET_BG;
+        } else {
+            backgroundColor = isLighterPlace ? LIGHTER_PLACE_COLOR : DARKER_PLACE_COLOR;
+        }
 
         if (piece == null) {
-            if (isPossibleMove) {
-                System.out.print(background + POSSIBLE_MOVE_COLOR + placeholder + " ");
+            if (hasPossibleMove) {
+                System.out.print(backgroundColor + POSSIBLE_MOVE_COLOR + placeholder + " ");
                 return;
             }
-            System.out.print(background + POSSIBLE_MOVE_COLOR + placeholder + " ");
+            System.out.print(backgroundColor + POSSIBLE_MOVE_COLOR + placeholder + " ");
             return;
         }
+
+
+        System.out.print(ANSI_RESET + backgroundColor + " " + getPieceFormating(piece, hasPossibleMove) + " " + ANSI_RESET);
+    }
+
+    private static String getPieceFormating(ChessPiece piece, boolean isPossibleMove) {
 
         String fontColor = piece.getColor() == Color.WHITE ? WHITE_PIECES_COLOR : BLACK_PIECES_COLOR;
         fontColor = isPossibleMove ? RED_UNDERLINED : fontColor;
 
-        System.out.print(ANSI_RESET + background  + " " + fontColor + piece + ANSI_RESET + background + " ");
+        return fontColor + getChessPieceUnicode(piece);
+    }
+
+
+    private static String getChessPieceUnicode (ChessPiece piece) {
+
+        int unicode = 0x265A;
+
+        switch (piece.getType()) {
+            case "King":
+                break;
+            case "Queen":
+                unicode += 1;
+                break;
+            case "Rook":
+                unicode += 2;
+                break;
+            case "Bishop":
+                unicode += 3;
+                break;
+            case "Knight":
+                unicode += 4;
+                break;
+            case "Pawn":
+                unicode += 5;
+                break;
+        }
+
+        return String.valueOf((char) unicode);
     }
 
     private static void printCapturedPieces(List<ChessPiece> capturedPieces) {
